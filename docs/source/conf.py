15f7e2f2e9d7b856dc6efea7b7719e4d8f189c12
Yamuna Krishnamurthy <yamuna@nyu.edu>
2017-04-10 20:09:51 -0400

    Using the default html template

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'dd_api'
-copyright = u'2017, Yamuna Krishnamurthy'
+copyright = u'2017, New York University'
 author = u'Yamuna Krishnamurthy'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -90,7 +90,7 @@ todo_include_todos = True
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
