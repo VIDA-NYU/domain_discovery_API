477d79bbe7fa6cdcfab67e5490e0780454b697d1
Yamuna Krishnamurthy <yamunak@gmail.com>
2017-04-26 00:25:59 -0400

    Changed htmlhelp_basename

--- docs/conf.py
+++ docs/conf.py
@@ -103,7 +103,7 @@ html_static_path = ['_static']
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'dd_apidoc'
+htmlhelp_basename = 'Domain Discovery API'
 
 
 # -- Options for LaTeX output ---------------------------------------------
