028dc209250273a17318e63132e8d9c0971e61f4
Yamuna Krishnamurthy <yamunak@gmail.com>
2017-07-09 22:39:40 -0400

    moved exec_query to misc_queries

--- elastic/search_documents.py
+++ elastic/search_documents.py
@@ -306,23 +306,6 @@ def field_exists(field, fields, pagesCount, es_index='memex', es_doc_type='page'
 
     return results
 
-def exec_query(query, fields, start=0, pagesCount=100, es_index='memex', es_doc_type='page', es=None):
-    if es is None:
-        es = default_es
-
-    query = query
-    query["fields"] = fields
-
-    res = es.search(body=query, index=es_index, doc_type=es_doc_type, from_=start, size=pagesCount, request_timeout=30)
-    hits = res['hits']['hits']
-
-    results = []
-    for hit in hits:
-        fields = hit['fields']
-        fields['id'] = hit['_id']
-        results.append(fields)
-
-    return {"total":res["hits"]["total"], "results":results}
 
 if __name__ == "__main__":
     print sys.argv[1:]
