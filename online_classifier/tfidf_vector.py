3fc3abee3d098e35fee965dcf1f95d978af52a01
Yamuna Krishnamurthy <yamunak@gmail.com>
2017-07-14 19:29:54 -0400

    Do not store data or tfidfarray. They should only be generated on request. Removed unused methods

--- online_classifier/tfidf_vector.py
+++ online_classifier/tfidf_vector.py
@@ -9,12 +9,8 @@ class tfidf_vectorizer(tf_vectorizer):
     def __init__(self, convert_to_ascii=False, max_features= 10000, ngram_range=(1,1)):
         self.tfidf_transformer = None
         tf_vectorizer.__init__(self, convert_to_ascii, max_features, ngram_range)
-        self.corpus = None
-        self.tfidfArray = None
-        self.data = None
         
     def tfidf(self, data):
-        self.data = data
         [X_counts, features] = self.vectorize(data)
         if self.tfidf_transformer is None:
             self.tfidf_transformer = TfidfTransformer()
@@ -22,19 +18,15 @@ class tfidf_vectorizer(tf_vectorizer):
         else:
             X = self.tfidf_transformer.transform(X_counts)
 
-        self.corpus = features
-        self.tfidfArray = X
-        
         return [X, X_counts, features]
-    
-    def getTerms(self, indices):
-        return [self.corpus[x] for x in indices]
 
-    def getTfidfArray(self):
-        return [None, self.corpus, self.tfidfArray]
+    @staticmethod
+    def getTerms(corpus, indices):
+        return [corpus[x] for x in indices]
 
-    def getTopTerms(self,top):
-        N = len(self.data)
-        avg = np.sum(self.tfidfArray, axis=0)
+    @staticmethod
+    def getTopTerms(tfidfArray, corpus, num_docs, top):
+        N = num_docs
+        avg = np.sum(tfidfArray, axis=0)
         sortedAvgIndices = np.argsort(np.array(avg)[0])[::-1]
-        return [self.corpus[i] for i in sortedAvgIndices[0:top]]
+        return [corpus[i] for i in sortedAvgIndices[0:top]]
